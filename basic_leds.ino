e1385beff992948f92573b47fc74f246b0236014
Thomas Shaw <tshaw258@student.ubc.ca>
2024-07-03 19:42:53 -0700

    Hazards LED testing with button

--- basic_leds.ino
+++ basic_leds.ino
@@ -3,24 +3,47 @@
 #define LEFT_SIGNAL 7
 #define RIGHT_SIGNAL 8
 
+#define HAZARDS_BUTTON 3
+
+bool hazardsOn = false;
+int recordedTime = millis();
 void setup() {
   // put your setup code here, to run once:
   pinMode(HEADLIGHTS, OUTPUT);
   pinMode(BRAKELIGHTS, OUTPUT);
   pinMode(LEFT_SIGNAL, OUTPUT);
   pinMode(RIGHT_SIGNAL, OUTPUT);
+  pinMode(HAZARDS_BUTTON, INPUT_PULLUP);
+  
+  digitalWrite(HEADLIGHTS, HIGH);
+  digitalWrite(BRAKELIGHTS, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(HEADLIGHTS, HIGH);
-  digitalWrite(BRAKELIGHTS, LOW);
-  digitalWrite(LEFT_SIGNAL, HIGH);
-  digitalWrite(RIGHT_SIGNAL, LOW);
-  delay(1000);
-  digitalWrite(HEADLIGHTS, LOW);
-  digitalWrite(BRAKELIGHTS, HIGH);
   digitalWrite(LEFT_SIGNAL, LOW);
-  digitalWrite(RIGHT_SIGNAL, HIGH);
-  delay(1000);
+  digitalWrite(RIGHT_SIGNAL, LOW);
+
+  if (digitalRead(HAZARDS_BUTTON) == LOW) {
+    hazardsOn = true;
+    recordedTime = millis();
+
+    while (digitalRead(HAZARDS_BUTTON) == LOW) {
+      setHazards(hazardsOn);
+      if (millis() - recordedTime >= 750) {
+        hazardsOn = !hazardsOn;
+        recordedTime = millis();
+      }
+    }
+  }
+}
+
+void setHazards(bool hazardsOn) {
+  if (hazardsOn) {
+    digitalWrite(LEFT_SIGNAL, HIGH);
+    digitalWrite(RIGHT_SIGNAL, HIGH);
+  } else {
+    digitalWrite(LEFT_SIGNAL, LOW);
+    digitalWrite(RIGHT_SIGNAL, LOW);
+  }
 }
