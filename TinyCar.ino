f5453d21a3e087f308e69746f3c5388902c23f76
Thomas Shaw <83386554+ThomasAShaw@users.noreply.github.com>
2024-07-20 13:53:02 -0700

    Turn signal switches integrated with hazards

--- TinyCar.ino
+++ TinyCar.ino
@@ -1,43 +1,70 @@
 #define HEADLIGHTS 3
 #define BRAKELIGHTS 9
-#define LEFT_SIGNAL 5
-#define RIGHT_SIGNAL 6
+#define LEFT_SIGNAL_LIGHT 5
+#define RIGHT_SIGNAL_LIGHT 6
 #define HAZARDS_BUTTON 2
 #define GAS_PEDAL_DIGITAL 12 // Testing as digital, will be switched to analog eventually
 #define BRAKE_PEDAL_DIGITAL 13 // Testing as digital, will be switched to analog eventually
 #define HEADLIGHTS_LOW_SWITCH 7
 #define HEADLIGHTS_HIGH_SWITCH 8
+#define LEFT_SIGNAL_SWITCH 10
+#define RIGHT_SIGNAL_SWITCH 11
+
+#define SIGNAL_TIMING_MS 750
 
 bool hazardsToggledOn = false;
-bool hazardsLightsOn = false;
+bool leftSignalToggledOn = false;
+bool rightSignalToggledOn = false;
+bool signalLightsOn = false;
 bool hazardButtonState = HIGH;
-unsigned long recordedTime = 0;
+unsigned long signalTime = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(HEADLIGHTS, OUTPUT);
   pinMode(BRAKELIGHTS, OUTPUT);
-  pinMode(LEFT_SIGNAL, OUTPUT);
-  pinMode(RIGHT_SIGNAL, OUTPUT);
+  pinMode(LEFT_SIGNAL_LIGHT, OUTPUT);
+  pinMode(RIGHT_SIGNAL_LIGHT, OUTPUT);
   pinMode(HAZARDS_BUTTON, INPUT_PULLUP);
   pinMode(GAS_PEDAL_DIGITAL, INPUT_PULLUP);
   pinMode(BRAKE_PEDAL_DIGITAL, INPUT_PULLUP);
   pinMode(HEADLIGHTS_LOW_SWITCH, INPUT_PULLUP);
   pinMode(HEADLIGHTS_HIGH_SWITCH, INPUT_PULLUP);
+  pinMode(LEFT_SIGNAL_SWITCH, INPUT_PULLUP);
+  pinMode(RIGHT_SIGNAL_SWITCH, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(LEFT_SIGNAL, 0);
-  analogWrite(RIGHT_SIGNAL, 0);
-
-  handleHazardsButton();
+  handleTurnSignals();
   handleHeadlights();
   handleGasPedal();
   handleBrakePedal();
 }
 
-void handleHazardsButton(void) {
+void handleTurnSignals(void) {
+  if (!hazardsToggledOn && !leftSignalToggledOn && !rightSignalToggledOn) {
+    // Ensures timing resets if a switch/button is toggled on
+    signalTime = millis() - SIGNAL_TIMING_MS;
+  }
+
+  readSignalInputs();
+  analogWrite(LEFT_SIGNAL_LIGHT, (signalLightsOn && (hazardsToggledOn || leftSignalToggledOn)) ? 255 : 0);
+  analogWrite(RIGHT_SIGNAL_LIGHT, (signalLightsOn && (hazardsToggledOn || rightSignalToggledOn)) ? 255 : 0);
+
+  if (hazardsToggledOn || leftSignalToggledOn || rightSignalToggledOn) {
+    if (millis() - signalTime >= SIGNAL_TIMING_MS) {
+      signalLightsOn = !signalLightsOn;
+      signalTime = millis();
+    }
+  }
+}
+
+void readSignalInputs(void) {
+  // Individual switches
+  leftSignalToggledOn = digitalRead(LEFT_SIGNAL_SWITCH) == LOW;
+  rightSignalToggledOn = digitalRead(RIGHT_SIGNAL_SWITCH) == LOW;
+
+  // Hazard light button reading (press toggles it)
   bool hazardButtonReading = digitalRead(HAZARDS_BUTTON);
 
   if (hazardButtonState != hazardButtonReading) {
@@ -45,27 +72,8 @@ void handleHazardsButton(void) {
 
     if (hazardButtonState == LOW) {
       hazardsToggledOn = !hazardsToggledOn;
-      hazardsLightsOn = hazardsToggledOn;
-      recordedTime = millis();
+      signalLightsOn = signalLightsOn;
     }
-  }
-
-  setHazards(hazardsLightsOn);
-  if (hazardsToggledOn) {
-    if (millis() - recordedTime >= 750) {
-      hazardsLightsOn = !hazardsLightsOn;
-      recordedTime = millis();
-    }
-  }
-}
-
-void setHazards(bool hazardsOn) {
-  if (hazardsOn) {
-    analogWrite(LEFT_SIGNAL, 255);
-    analogWrite(RIGHT_SIGNAL, 255);
-  } else {
-    analogWrite(LEFT_SIGNAL, 0);
-    analogWrite(RIGHT_SIGNAL, 0);
   }
 }
 
